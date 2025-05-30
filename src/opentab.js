55b266a91256e53e1f48e2a6ca38a8c695be1a07
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-05-30 11:57:54 +0200

    Added style file and opentab js from restaurant

---
+++ src/opentab.js
@@ -0,0 +1,20 @@
+export default function openTab(evt, tabName){
+    var i;
+    var tabcontent;
+    var tablinks;
+
+    tabcontent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+};
+
