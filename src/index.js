34079e6d05ac41ddb720cf533fce1a51a5c33bb8
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-06-02 10:56:21 +0200

    Moved sidebar logic in its own sidebar.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./styles.css";
+import openTab from "./opentab";
+window.openTab = openTab ;
+
+import { openNav, closeNav } from "./sidebar";
+window.openNav = openNav ;
+window.closeNav = closeNav ;
---
+++ src/sidebar.js
@@ -0,0 +1,9 @@
+export function openNav() {
+    document.getElementById("tab").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+    };
+        
+export function closeNav() {
+    document.getElementById("tab").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    };
