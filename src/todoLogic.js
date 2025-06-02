3c50e97879687504c5d3c14b23c30b5c0b0aa971
Noceolo <176486244+Noceolo@users.noreply.github.com>
2025-06-02 11:03:15 +0200

    Added comments

---
+++ src/todoLogic.js
@@ -0,0 +1,7 @@
+// here i'm gonna build all the logic bits, before organizing them by goals/cleaning them
+
+// todos creation : 
+
+    // Object class for todos
+    // creating new todo from form
+    // 
